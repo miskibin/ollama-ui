97a735ab58bda92520626bada7b601cac48a3ab6
Michal Skibinski <michal.skibinski@nokia.com>
2024-10-16 11:41:25 +0200

    Refactor profile pages to handle missing user picture and name

--- app/profile-client/page.tsx
+++ app/profile-client/page.tsx
@@ -11,7 +11,7 @@ export default function ProfileClient() {
   return (
     user && (
       <div>
-        <img src={user.picture} alt={user.name} />
+        {user.picture && user.name && <img src={user.picture} alt={user.name} />}
         <h2>{user.name}</h2>
         <p>{user.email}</p>
       </div>
--- app/profile-server/page.tsx
+++ app/profile-server/page.tsx
@@ -1,7 +1,8 @@
 import { getSession } from "@auth0/nextjs-auth0";
 
 export default async function ProfileServer() {
-  const { user } = await getSession();
+  const session = await getSession();
+  const user = session?.user;
 
   return (
     user && (
